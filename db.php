46092b2942485be41082102c6049470dfa1cc068
houssamlambara <lambarahoussam@gmail.com>
2024-12-26 14:50:43 +0100

    file

---
+++ db.php
@@ -0,0 +1,33 @@
+<?php 
+ class dataBase {
+  private static $instance = null;//type dataBase
+  private $connection;//type PDO
+
+  private function __construct() {
+      $this->connection = new PDO("mysql:host=localhost;dbname=gestion_reservations2", "root", "");
+  }
+
+  public static function getInstance() {
+      if (self::$instance == null) {
+        try {
+          self::$instance = new dataBase();
+          self::$instance->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+          echo "Connected successfully";
+        } catch (PDOException $e) {
+          echo "Connection failed: " . $e->getMessage();
+        }
+
+      }
+      return self::$instance;
+  }
+
+  public function getConnection() {
+      return $this->connection;
+  }
+
+ 
+}
+
+
+
+?>
