2b8c78aa11b7667f52e08cc9ec7bf40e5f39f3ac
houssamlambara <lambarahoussam@gmail.com>
2024-12-27 13:13:27 +0100

    add

--- front_end/index.php
+++ front_end/index.php
@@ -118,7 +118,7 @@
     </div>
   </div>
   <div class="flex justify-center mt-8 mb-8">
-    <a href="./activities.php" class="w-full md:w-auto bg-orange-500 text-white py-4 px-8 rounded-lg font-semibold hover:bg-gradient-to-l hover:bg-orange-600 transition duration-300 text-center block">Réserver Une Activité</a>
+    <a href="../front_end/reservation.php" class="w-full md:w-auto bg-orange-500 text-white py-4 px-8 rounded-lg font-semibold hover:bg-gradient-to-l hover:bg-orange-600 transition duration-300 text-center block">Réserver Une Activité</a>
   </div>
 </section>
 
