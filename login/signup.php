70537f3d1a5bfd262ddf7c1e05656b7d7d080cfa
houssamlambara <lambarahoussam@gmail.com>
2024-12-27 15:21:07 +0100

    signup & signin

--- login/signup.php
+++ login/signup.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdn.tailwindcss.com"></script>
-    <title>Le Chef - Alain Passard</title>
+    <title>Sign UP</title>
 </head>
 <body class="bg-gray-50 text-gray-800">
 
